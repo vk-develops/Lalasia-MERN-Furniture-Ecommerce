45c1f9b9dcd66dd2d97669b7a8f36b3d70a991b9
vk-develops <vimalkumarv291@gmail.com>
2024-02-01 20:32:34 +0530

    chore: App initialized

---
+++ Backend/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+
+//App init
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+//HTTP GET Method Test
+app.get("/", (req, res) => {
+    res.status(200).json({ success: true, message: "HTTP Method Success!" });
+});
+
+//App listen
+app.listen(PORT, () => {
+    console.log(`Server started and running on http://localhost:${PORT}`);
+});
