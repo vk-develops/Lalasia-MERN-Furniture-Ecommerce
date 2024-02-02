603224c096eb2e624f6ad36fddb9288af0c09a1c
vk-develops <vimalkumarv291@gmail.com>
2024-02-02 21:28:35 +0530

    chore: Added the protection route

---
+++ Backend/Middlewares/authMiddleware.js
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+
+const protect = asyncHandler(async (req, res) => {
+    let token;
+
+    //Extracting the token from the cookie
+    token = req.cookies.jwt;
+
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+            req.user = await User.findById(decoded.userId).select("-password");
+
+            next();
+        } catch (err) {
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized access, Invalid tokens.",
+            });
+        }
+    } else {
+        return res.status(401).json({
+            success: false,
+            message: "Unauthorized access, No tokens.",
+        });
+    }
+});
+
+//Export
+export { protect };
