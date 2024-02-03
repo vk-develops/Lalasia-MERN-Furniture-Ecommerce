3f75c7eb356fc0e86afbb0c95889c89c8113a7ea
vk-develops <vimalkumarv291@gmail.com>
2024-02-03 19:47:28 +0530

    feat: Added the header and footer and creted the layout of the app

---
+++ Frontend/src/Pages/Home/HomePage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { styles } from "../../Styles/styles";
+
+const HomePage = () => {
+    return <div className={`${styles.navLink}`}>Product</div>;
+};
+
+export default HomePage;
---
+++ Frontend/src/Styles/styles.js
@@ -0,0 +1,8 @@
+const styles = {
+    navLink: "font-eduoxusSans text-sm font-medium",
+    layout: "max-w-5xl mx-auto p-5",
+    navButton: `px-4 py-3 bg-primaryColor inline-block text-screenColor1 font-eduoxusSans font-medium text-sm max-mobile:text-xs`,
+};
+
+//Export
+export { styles };
