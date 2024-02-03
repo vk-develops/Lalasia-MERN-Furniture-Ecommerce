b1884f3334dde78f88268b0768fdd45a9f1eb4bd
vk-develops <vimalkumarv291@gmail.com>
2024-02-03 23:02:38 +0530

    chore: Added the neccessary files and folders

--- Frontend/src/Router/Router.jsx
+++ Frontend/src/Router/Router.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../Pages/Home/HomePage";
 import Layout from "../Components/Layout";
+import ProductsPage from "../Pages/Products/ProductsPage";
+import RegisterPage from "../Pages/Auth/RegisterPage";
+import LoginPage from "../Pages/Auth/LoginPage";
+import VerifyPage from "../Pages/Auth/VerifyPage";
 
 const Router = () => {
     return (
@@ -18,10 +22,26 @@ const Router = () => {
 
                 {/* Account Routes */}
                 <Route path="account">
-                    <Route path="register" />
-                    <Route path="login" />
-                    <Route path="verify" />
+                    <Route
+                        path="register"
+                        element={<RegisterPage />}
+                    />
+                    <Route
+                        path="login"
+                        element={<LoginPage />}
+                    />
+                    <Route
+                        path="verify"
+                        element={<VerifyPage />}
+                    />
                     <Route path="reset-password" />
+                </Route>
+
+                <Route path="products">
+                    <Route
+                        index
+                        element={<ProductsPage />}
+                    />
                 </Route>
             </Route>
         </Routes>
