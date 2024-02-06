2a9d3e29da983898124b56d5a7a405219ea61765
vk-develops <vimalkumarv291@gmail.com>
2024-02-06 22:36:45 +0530

    chore: Initialized the redux store

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import Router from "./Router/Router";
+import store from "./App/store";
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Router />
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Router />
+            </BrowserRouter>
+        </Provider>
     );
 };
 
