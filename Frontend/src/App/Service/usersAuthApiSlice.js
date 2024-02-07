ac58607b89fb4a0bd7dc280188655805c6f9fbd2
vk-develops <vimalkumarv291@gmail.com>
2024-02-07 20:31:07 +0530

    feat: Added the users auth api slice frontend

---
+++ Frontend/src/App/Service/usersAuthApiSlice.js
@@ -0,0 +1,41 @@
+import { USERS_AUTH_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+export const usersAuthApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        register: builder.mutation({
+            query: (data) => ({
+                url: `${USERS_AUTH_URL}/register`,
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: data,
+            }),
+        }),
+
+        login: builder.mutation({
+            query: (data) => ({
+                url: `${USERS_AUTH_URL}/login`,
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: data,
+            }),
+        }),
+
+        logout: builder.mutation({
+            query: () => ({
+                url: `${USERS_AUTH_URL}/logout`,
+                method: "POST",
+                credentials: "include",
+            }),
+        }),
+    }),
+});
+
+export const { useRegisterMutation, useLoginMutation, useLogoutMutation } =
+    usersAuthApiSlice;
