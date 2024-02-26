821a4f24c4f32ffaae1309dcdaa0a3ea1254b9c2
vk-develops <vimalkumarv291@gmail.com>
2024-02-26 17:44:15 +0530

    feat: Added the admin page UI

--- Frontend/src/Pages/Admin/AdminPage.jsx
+++ Frontend/src/Pages/Admin/AdminPage.jsx
@@ -3,7 +3,9 @@ import { styles } from "../../Styles/styles";
 import { Link } from "react-router-dom";
 import { useGetAllProductsQuery } from "../../App/Service/adminProductApiSlice";
 import Loader from "../../Components/Loader";
-import ProductCard from "../../Components/ProductCard";
+import { IoMdEye } from "react-icons/io";
+import { FiEdit } from "react-icons/fi";
+import { MdDeleteOutline } from "react-icons/md";
 
 const AdminPage = () => {
     const [products, setProducts] = useState(null);
@@ -38,16 +40,114 @@ const AdminPage = () => {
                     <h2 className={`${styles.secondaryText}`}>
                         Furniture Products
                     </h2>
-                    <div className="grid grid-cols-3 gap-10 pt-10">
-                        {products &&
-                            products.map((product) => (
-                                <Link
-                                    to={`update-product/${product._id}`}
-                                    key={product._id}
+                    <div className="max-w-full overflow-x-scroll">
+                        <table className="w-full border border-paragraphColor">
+                            <thead>
+                                <tr
+                                    style={{
+                                        color: "#ffffff",
+                                        fontWeight: 500,
+                                    }}
+                                    className="bg-primaryColor text-white"
                                 >
-                                    <ProductCard product={product} />
-                                </Link>
-                            ))}
+                                    <th className="border border-paragraphColor p-4">
+                                        ID
+                                    </th>
+                                    <th className="border border-paragraphColor p-4">
+                                        Name
+                                    </th>
+                                    <th className="border border-paragraphColor p-4">
+                                        SubTitle
+                                    </th>
+                                    <th className="border border-paragraphColor p-4">
+                                        Description
+                                    </th>
+                                    <th className="border border-paragraphColor p-4">
+                                        Price
+                                    </th>
+                                    <th className="border border-paragraphColor p-4">
+                                        Rating
+                                    </th>
+                                    <th className="border border-paragraphColor p-4">
+                                        Common type
+                                    </th>
+                                    <th className="border border-paragraphColor p-4">
+                                        View
+                                    </th>
+                                    <th className="border border-paragraphColor p-4">
+                                        Update
+                                    </th>
+                                    <th className="border border-paragraphColor p-4">
+                                        Delete
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {products &&
+                                    products.map((product) => (
+                                        <tr className="text-center">
+                                            <td className="hover:bg-screenColor2 border whitespace-nowrap p-4 border-paragraphColor">
+                                                {product._id}
+                                            </td>
+                                            <td className="hover:bg-screenColor2 border whitespace-nowrap p-4 border-paragraphColor">
+                                                {product.name}
+                                            </td>
+                                            <td className="hover:bg-screenColor2 border whitespace-nowrap p-4 border-paragraphColor">
+                                                {product.subTitle}
+                                            </td>
+                                            <td className="hover:bg-screenColor2 border whitespace-nowrap p-4 border-paragraphColor">
+                                                {`${product.description.slice(
+                                                    0,
+                                                    50
+                                                )}...`}
+                                            </td>
+                                            <td className="hover:bg-screenColor2 border whitespace-nowrap p-4 border-paragraphColor">
+                                                {product.price}
+                                            </td>
+                                            <td className="hover:bg-screenColor2 border whitespace-nowrap p-4 border-paragraphColor">
+                                                {product.starRating}
+                                            </td>
+                                            <td className="hover:bg-screenColor2 border whitespace-nowrap p-4 border-paragraphColor">
+                                                {product.commonType}
+                                            </td>
+                                            <td className="hover:bg-screenColor2 border flex items-center justify-center whitespace-nowrap p-4 border-paragraphColor">
+                                                <Link>
+                                                    {" "}
+                                                    <IoMdEye
+                                                        style={{
+                                                            color: "#518581",
+                                                        }}
+                                                        size={24}
+                                                    />
+                                                </Link>
+                                            </td>
+                                            <td className="hover:bg-screenColor2 border whitespace-nowrap p-4 border-paragraphColor">
+                                                <Link>
+                                                    {" "}
+                                                    <FiEdit
+                                                        style={{
+                                                            color: "#518581",
+                                                            marginLeft: 15,
+                                                        }}
+                                                        size={24}
+                                                    />
+                                                </Link>
+                                            </td>
+                                            <td className="hover:bg-screenColor2 flex items-center justify-center flex-row border whitespace-nowrap p-4 border-paragraphColor">
+                                                <Link>
+                                                    {" "}
+                                                    <MdDeleteOutline
+                                                        style={{
+                                                            color: "#518581",
+                                                        }}
+                                                        size={24}
+                                                    />
+                                                </Link>
+                                            </td>
+                                        </tr>
+                                    ))}
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </section>
