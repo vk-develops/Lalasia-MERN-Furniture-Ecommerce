29b5890817318f0a4b010b8072aa380492b4d495
vk-develops <vimalkumarv291@gmail.com>
2024-02-29 22:21:35 +0530

    feat: Added the exact back redirect

--- Frontend/src/Components/BackButton.jsx
+++ Frontend/src/Components/BackButton.jsx
@@ -6,12 +6,14 @@ import { useLocation } from "react-router-dom";
 const BackButton = ({ backTo, buttonText }) => {
     const location = useLocation();
 
-    // console.log(location);
+    // console.log(location.state.from.split("/")[1]);
+
+    const back = location.state ? location.state.from : "/";
 
     return (
         <div className="inline-block">
             <Link
-                to={`${backTo}`}
+                to={`${back}`}
                 className=" bg-placeholderColor border-[.5px] border-[#8885] px-8 py-3 rounded-sm flex items-center justify-center gap-2"
             >
                 <MdArrowBack
@@ -19,7 +21,7 @@ const BackButton = ({ backTo, buttonText }) => {
                     color="#555"
                 />
                 <h4 className="text-base font-eduoxusSans font-medium text-[#555]">
-                    {buttonText}
+                    Go to {back != "/" ? back.split("/")[1] : "Home"} page
                 </h4>
             </Link>
         </div>
