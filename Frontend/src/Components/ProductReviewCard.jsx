a9d9755cd4ff9f27529549305a9b50f735ebf5dc
vk-develops <vimalkumarv291@gmail.com>
2024-03-15 22:35:44 +0530

    feat: Added the reviews UI

--- Frontend/src/Components/ProductReviewCard.jsx
+++ Frontend/src/Components/ProductReviewCard.jsx
@@ -4,7 +4,7 @@ const ProductReviewCard = ({ productReview }) => {
     return (
         <div className="w-2/4 p-10">
             <div>
-                <div>
+                <div className="flex items-start justify-start gap-5">
                     <img
                         src={`${
                             productReview.userImage
@@ -12,11 +12,21 @@ const ProductReviewCard = ({ productReview }) => {
                                 : "https://w7.pngwing.com/pngs/178/595/png-transparent-user-profile-computer-icons-login-user-avatars-thumbnail.png"
                         }`}
                         alt="Profile Image"
-                        className="w-12 rounded-full"
+                        className="w-[40px] rounded-full"
                     />
-                    <div>
-                        <h6>{productReview.userName}</h6>
-                        <h6>{productReview.c}</h6>
+                    <div className=" -mt-[3px]">
+                        <div className="flex items-start justify-start flex-col gap-[2px]">
+                            <h6 className="text-base font-eduoxusSans font-medium text-primaryColor">
+                                {productReview.userName}
+                            </h6>
+                            <h6 className="text-[12px] font-eduoxusSans font-noraml text-paragraphColor">
+                                {productReview.userEmail}
+                            </h6>
+                        </div>
+
+                        <p className="pt-3 text-base font-eduoxusSans text-titleColor font-medium">
+                            {productReview.comment}
+                        </p>
                     </div>
                 </div>
             </div>
